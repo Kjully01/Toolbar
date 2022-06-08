1ec3eb1e4a94700c6da5c7a1f979427200ff716a
Karen Jully <karenjully.kr15@gmail.com>
2022-06-08 09:15:03 -0300

    implementação do "voltar" da toolbar

--- app/src/main/java/br/com/toolbar/HomeActivity.kt
+++ app/src/main/java/br/com/toolbar/HomeActivity.kt
@@ -2,10 +2,34 @@ package br.com.toolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import br.com.toolbar.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.title = ""
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
