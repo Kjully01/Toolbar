da6c2d0d6d24195fbdf4d713635f46cf27e707bb
Karen Jully <karenjully.kr15@gmail.com>
2022-06-08 10:13:42 -0300

    configurando gif no splash

--- app/src/main/java/br/com/toolbar/SplashActivity.kt
+++ app/src/main/java/br/com/toolbar/SplashActivity.kt
@@ -5,16 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import br.com.toolbar.databinding.ActivitySplashBinding
+import com.bumptech.glide.Glide
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashActivity, HomeActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
+
+        showGif(binding.root)
+    }
+
+    private fun showGif(view: View) {
+        val imageView: ImageView = binding.imgSplash
+        Glide.with(this).load(R.drawable.programming_computer).into(imageView)
     }
 }
